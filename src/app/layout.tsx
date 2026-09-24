1f870820abbfd22d445091dcfbdce3ac6accb5ed
Edxonra <edxonrodrigueza@gmail.com>
2026-09-24 00:46:26 -0600

    Fixing style for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import type { Viewport } from 'next'
 import './globals.css'
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+}
 
 export default function RootLayout({
   children,
